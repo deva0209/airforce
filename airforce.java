fae82dbddcc56468092b57aea72655b6ae05df63
deva0209 <sgnandev@gmail.com>
2022-12-22 18:41:26 +0530

    congradulations

---
+++ airforce.java
@@ -0,0 +1,5 @@
+class airforce{
+    public static void main(String[] args) {
+        System.out.println("congradulations gnan"); 
+    }
+}
---
+++ gnan.java
@@ -0,0 +1,5 @@
+class gnan {
+    public static void main(String[] args) {
+        System.out.println("congradulations gnan"); 
+    }
+}
